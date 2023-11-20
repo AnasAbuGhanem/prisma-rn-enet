0d823b8930d0a461eda32063e2c21ca4d1dbba07
AnasAbuGhanem <anas.abu.ghanem@hotmail.com>
2023-11-20 12:17:27 +0100

    key change

--- Key-gen.py
+++ Key-gen.py
@@ -1,5 +1,8 @@
 import requests
 import json
+from getpass import getpass
+import xmltodict
+
 
 user="sehaani01"
 password = getpass(prompt='Input your password: ') # the default prompt is 'Password: 
@@ -7,3 +10,7 @@ password = getpass(prompt='Input your password: ') # the default prompt is 'Pass
 url_security='https://panorama.ngninfra.net/api/?type=keygen'
 params={'type':'keygen','user':user,'password':password}
 get_key=requests.get(url_security, params=params, verify=False)
+
+data=xmltodict.parse(get_key.content)
+api_key=data['response']['result']['key']
+print('The new API key is: ', api_key)
--- cred.py
+++ cred.py
@@ -6,7 +6,3 @@ user="sehaani01"
 password = getpass(prompt='Input your password: ') # the default prompt is 'Password: 
 
 key='LUFRPT05N3g0SFJnOGFmeE5tNWRGQXBzRWNDTVBoWVU9bnZhOXg4c2g5ZnJtT2ZMY2pUeGIyUW9jaHZWdlRkTDZJT0tkUW4rcjFWN1VxUlM5WjlGWkxFeVRFaEhtZllaS08vZE4vcm96RnZFR3V2WCtueVBVRnc9PQ=='
-
-##url_security='https://panorama.ngninfra.net/api/?type=keygen'
-##params={'type':'keygen','user':user,'password':password}
-##get_key=requests.get(url_security, params=params, verify=False)
